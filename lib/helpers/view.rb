c846ff6c0b3eaa1e41ca5580675375a98ddacbb1
David FRANCOIS <david.francois@webflows.fr>
2010-09-12 12:25:11 +0200

    Rails 3 go

--- lib/helpers/view.rb
+++ lib/helpers/view.rb
@@ -4,7 +4,7 @@ module FrCalendar
       def fr_calendar_includes
         js = %w{calendar calendar-setup lang/calendar-fr ../jscalendar-helpers}.inject("") { |m, i| m + javascript_include_tag("fr_calendar/default/jscalendar-1.0/#{i}") }
         css = stylesheet_link_tag "../javascripts/fr_calendar/default/jscalendar-1.0/skins/aqua/theme.css"         
-        js + css
+        (js + css).html_safe
       end
     end
   end
